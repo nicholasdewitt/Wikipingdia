e197e1dc6ce3a03b646a3f318c3be3606e67a9a9
NicholasDeWitt <ndewitt@umd.edu>
2018-05-14 21:39:19 -0400

    added some php stuff

--- displayprereqs.php
+++ displayprereqs.php
@@ -1,5 +1,5 @@
 <!-- This is the PHP to gather all of the data from the prereqs table. It still needs to look pretty and be embedded in our stylesheet & html stuff	 -->
-	<?php 
+	<?php
 		include 'resources/bslinks.php';
 		$a = "SELECT c1.course_code, c1.course_name as course, c2.course_code AS pre_course_code, c2.course_name as prereq_name FROM courses c1 JOIN prereqs p ON c1.course_code = p.course_code JOIN courses c2 ON p.pre_course_code = c2.course_code;";
 		$row = null;
@@ -8,7 +8,7 @@
 		$prereq = $conn->query($a);
 		$ar = $conn->query($prereq);
 		$result = mysqli_query($conn, $a);
-		
+
 		echo "<table cellpadding='2' cellspacing='2' border='1' bgcolor='#dfdfdf' width='125%' align='left'>"; // start a table tag in the HTML
 
 
